3d328990f22084d5fb0e3311fd3538a765280a7c
AlenaYashkina <120055830+AlenaYashkina@users.noreply.github.com>
2023-01-16 20:33:01 +0100

    Add files via upload
    Исправлено:
    - Полиморфизм, инкапсуляция
    - getMySQLConnection в Util исправлен в 1 метод (было 2)
    - Импор пакетами исправлен на импорт отдельными классами
    - Statement заменен на PreparedStatement
    - System.out.println заменён на Logger в saveUser и getAllUsers (логгеры добавлены в слой UserService)
    - Принципы ООП и SOLID повторены
    - В Main поле UserService, а не UserDAO
    - getMySQLConnection  убрать в Main

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -13,17 +13,12 @@ public class Util {
         String userName = "root";
         String password = "root";
 
-        return getMySQLConnection(hostName, dbName, userName, password);
-    }
-
-    public static Connection getMySQLConnection(String hostName, String dbName,
-                                                String userName, String password) throws SQLException,
-            ClassNotFoundException {
-
         Class.forName("com.mysql.cj.jdbc.Driver");
         String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
         Connection conn = DriverManager.getConnection(connectionURL, userName,
                 password);
+
         return conn;
+
     }
 }
